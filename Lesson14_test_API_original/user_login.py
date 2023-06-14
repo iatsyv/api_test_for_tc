55113f6250e06c48c20474cef169f69cdb6626f8
o.yatsiv <e.enion@gmail.com>
2023-06-15 01:56:27 +0300

    ffff

--- Lesson14_test_API_original/user_login.py
+++
@@ -1,5 +0,0 @@
-class UserLogin():
-    def __init__(self, email, password, remember):
-        self.email = email
-        self.password = password
-        self.remember = remember
--- Lesson14_test_API_original/user_register.py
+++
@@ -1,7 +0,0 @@
-class UserRegister():
-    def __init__(self, name, lastName, email, password, repeatPassword):
-        self.name = name
-        self.lastName = lastName
-        self.email = email
-        self.password = password
-        self.repeatPassword = repeatPassword
